338abe7bb484ea2a5de199d26761221819a64174
Furkan Arslan <frkn__asl@windowslive.com>
2021-12-28 13:12:45 +0300

    Initial commit

---
+++ src/main/java/com/furkanarslan/appcenthw/AppcenthwApplication.java
@@ -0,0 +1,13 @@
+package com.furkanarslan.appcenthw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcenthwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcenthwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furkanarslan/appcenthw/AppcenthwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furkanarslan.appcenthw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcenthwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
