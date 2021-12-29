799b5e30df3b135bce45e3a736516ba8961f6222
Furkan Arslan <frkn__asl@windowslive.com>
2021-12-29 21:33:26 +0300

    docs: openAPI descriptions for endpoints

--- src/main/java/com/furkanarslan/appcenthw/AppcenthwApplication.java
+++ src/main/java/com/furkanarslan/appcenthw/AppcenthwApplication.java
@@ -1,13 +1,30 @@
 package com.furkanarslan.appcenthw;
 
+import com.furkanarslan.appcenthw.model.AppUser;
+import com.furkanarslan.appcenthw.service.UserService;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppcenthwApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppcenthwApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppcenthwApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveUser(new AppUser(null, "abc", "abc", "1234"));
+            userService.saveUser(new AppUser(null, "abc", "123", "1234"));
+
+        };
+    }
 }
--- src/main/java/com/furkanarslan/appcenthw/controller/TodoController.java
+++ src/main/java/com/furkanarslan/appcenthw/controller/TodoController.java
@@ -6,6 +6,11 @@ import com.furkanarslan.appcenthw.model.AppUser;
 import com.furkanarslan.appcenthw.model.TodoTask;
 import com.furkanarslan.appcenthw.service.TodoService;
 import com.furkanarslan.appcenthw.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +23,15 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Todos")
 @RequestMapping("/todo")
 public class TodoController {
     private final TodoService todoService;
     private final UserService userService;
     private final TodoMapper todoMapper;
 
+    @Operation(summary = "Get all todos", description = "Get all todos for the currently logged in user.")
+    @ApiResponse(responseCode = "200", description = "All todos for the user.")
     @GetMapping("")
     public ResponseEntity<List<TodoDto>> getTodos(Authentication authentication) {
         AppUser owner = userService.getUser(authentication.getName());
@@ -31,6 +39,10 @@ public class TodoController {
         return new ResponseEntity<>(todos, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a todo detail.", description = "Get a todo detail for the currently logged in user.")
+    @ApiResponse(responseCode = "200", description = "Todo with the given id if the user owns the content.")
+    @ApiResponse(responseCode = "403", description = "If user doesn't own the content.", content = @Content)
+    @ApiResponse(responseCode = "404", description = "Todo not found.", content = @Content)
     @GetMapping("/{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable Long id, Authentication authentication) throws AccessDeniedException, ResponseStatusException {
         AppUser owner = userService.getUser(authentication.getName());
@@ -40,14 +52,19 @@ public class TodoController {
         return new ResponseEntity<>(todoDto, HttpStatus.OK);
     }
 
+    @Operation(summary = "Create todo.", description = "Create a new todo and save it for the currently logged in user.")
+    @ApiResponse(responseCode = "200", description = "Returns the newly created todo object.")
     @PostMapping("")
-    public ResponseEntity<TodoDto> createTodo(@RequestBody TodoTask todo, Authentication authentication) {
+    public ResponseEntity<TodoDto> createTodo(@RequestBody TodoDto todo, Authentication authentication) {
         AppUser owner = userService.getUser(authentication.getName());
-        todo.setOwner(owner);
-        todoService.saveTodo(todo);
-        return new ResponseEntity<>(todoMapper.TodoTaskToTodoDto(todo), HttpStatus.CREATED);
+        TodoTask todoTask = todoMapper.TodoDtoToTodoTask(todo);
+        todoTask.setOwner(owner);
+        todoService.saveTodo(todoTask);
+        return new ResponseEntity<>(todoMapper.TodoTaskToTodoDto(todoTask), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update todo.", description = "Update details of a todo and save it for the currently logged in user.")
+    @ApiResponse(responseCode = "200", description = "Returns the newly updated todo object.")
     @PutMapping("/{id}")
     public ResponseEntity<TodoDto> updateTodo(@PathVariable Long id, Authentication authentication, @RequestBody TodoDto todo) {
         AppUser owner = userService.getUser(authentication.getName());
@@ -58,6 +75,8 @@ public class TodoController {
         return new ResponseEntity<>(todoMapper.TodoTaskToTodoDto(currentTodo), HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete todo.", description = "Delete a todo for the currently logged in user.")
+    @ApiResponse(responseCode = "204", description = "Returns nothing with a status code of 204.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removeTodo(@PathVariable Long id, Authentication authentication) {
         AppUser owner = userService.getUser(authentication.getName());
