e5dfba4122e390622b4f2137193fac6e6c268562
Furkan Arslan <frkn__asl@windowslive.com>
2021-12-29 13:29:56 +0300

    fix(auth): add username check for user registration

--- src/main/java/com/furkanarslan/appcenthw/controller/AuthController.java
+++ src/main/java/com/furkanarslan/appcenthw/controller/AuthController.java
@@ -3,11 +3,13 @@ package com.furkanarslan.appcenthw.controller;
 import com.furkanarslan.appcenthw.model.AppUser;
 import com.furkanarslan.appcenthw.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +18,11 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("register")
-    public ResponseEntity<AppUser> createUser(@RequestBody AppUser user) {
-        return ResponseEntity.ok().body(userService.saveUser(user));
+    public ResponseEntity<AppUser> createUser(@RequestBody AppUser newUser) throws ResponseStatusException {
+        AppUser user = userService.getUser(newUser.getUsername());
+        if (user != null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid username. User already exists.");
+        }
+        return new ResponseEntity<>(userService.saveUser(newUser), HttpStatus.CREATED);
     }
 }
