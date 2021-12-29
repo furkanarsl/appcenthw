7e19b51873b60c072e25a565c6f478e5db0f4351
Furkan Arslan <frkn__asl@windowslive.com>
2021-12-29 16:30:07 +0300

    feat(auth): add endpoint to get user info

--- src/main/java/com/furkanarslan/appcenthw/controller/AuthController.java
+++ src/main/java/com/furkanarslan/appcenthw/controller/AuthController.java
@@ -1,14 +1,14 @@
 package com.furkanarslan.appcenthw.controller;
 
+import com.furkanarslan.appcenthw.dto.AppUserDto;
+import com.furkanarslan.appcenthw.mapper.UserMapper;
 import com.furkanarslan.appcenthw.model.AppUser;
 import com.furkanarslan.appcenthw.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -16,6 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @PostMapping("register")
     public ResponseEntity<AppUser> createUser(@RequestBody AppUser newUser) throws ResponseStatusException {
@@ -24,5 +25,11 @@ public class AuthController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid username. User already exists.");
         }
         return new ResponseEntity<>(userService.saveUser(newUser), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<AppUserDto> userInfo(Authentication authentication) {
+        AppUser user = userService.getUser(authentication.getName());
+        return new ResponseEntity<>(userMapper.AppUserToUserDto(user), HttpStatus.OK);
     }
 }
