908c3a315cc466f9bb75c5dd6d3a64f86e6b4f7f
Furkan Arslan <frkn__asl@windowslive.com>
2021-12-31 18:21:43 +0300

    refactor: remove cmdrunner from main

--- src/main/java/com/furkanarslan/appcenthw/AppcenthwApplication.java
+++ src/main/java/com/furkanarslan/appcenthw/AppcenthwApplication.java
@@ -1,16 +1,7 @@
 package com.furkanarslan.appcenthw;
 
-import com.furkanarslan.appcenthw.model.AppUser;
-import com.furkanarslan.appcenthw.service.UserService;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.tags.Tag;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppcenthwApplication {
@@ -19,12 +10,4 @@ public class AppcenthwApplication {
         SpringApplication.run(AppcenthwApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner run(UserService userService) {
-        return args -> {
-            userService.saveUser(new AppUser(null, "abc", "abc", "1234"));
-            userService.saveUser(new AppUser(null, "abc", "123", "1234"));
-
-        };
-    }
 }
